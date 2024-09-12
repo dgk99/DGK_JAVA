0b9d380794a0a160bc1620a58281f20f60e927f8
MinGyu Kim <dooek1318@g.yju.ac.kr>
2024-09-12 16:22:02 +0900

    자바 수업

---
+++ src/lec240912/charstring.java
@@ -0,0 +1,10 @@
+package lec240912;
+
+public class charstring {
+
+    public static void main(String[] args) {
+
+        char bar = '1'; // '' -> char : 문자
+        String foo = "12"; // "" -> String : 문자열
+    }
+}
