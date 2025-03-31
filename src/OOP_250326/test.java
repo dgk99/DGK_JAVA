485819176a47aed4857c0731367bd61e071d7059
Kim Min Gyu <dooek1318@g.yju.ac.kr>
2025-03-31 15:50:32 +0900

    클래스 & 인스턴스

--- src/OOP_250326/test.java
+++ src/OOP_250326/test.java
@@ -1,26 +1,26 @@
-package OOP_250326;
-
-class ItemBox {
-    int numOfBoxes;
-    void push() {numOfBoxes++;}
-    void pop() {numOfBoxes--;}
-}
-class Foo {
-    ItemBox box;
-    Foo(ItemBox argBox) {box = argBox;}
-}
-class Pos {
-    ItemBox box;
-    Pos(ItemBox argBox) {box = argBox;}
-}
-public class test {
-    public static void main(String[] args) {
-        ItemBox box = new ItemBox();
-        Foo foo = new Foo(box);
-        Pos pos = new Pos(box);
-        foo.box.push();
-        foo.box.push();
-        pos.box.pop();
-        pos.box.pop();
-    }
-}
+//package OOP_250326;
+//
+//class ItemBox {
+//    int numOfBoxes;
+//    void push() {numOfBoxes++;}
+//    void pop() {numOfBoxes--;}
+//}
+//class Foo {
+//    ItemBox box;
+//    Foo(ItemBox argBox) {box = argBox;}
+//}
+//class Pos {
+//    ItemBox box;
+//    Pos(ItemBox argBox) {box = argBox;}
+//}
+//public class test {
+//    public static void main(String[] args) {
+//        ItemBox box = new ItemBox();
+//        Foo foo = new Foo(box);
+//        Pos pos = new Pos(box);
+//        foo.box.push();
+//        foo.box.push();
+//        pos.box.pop();
+//        pos.box.pop();
+//    }
+//}
