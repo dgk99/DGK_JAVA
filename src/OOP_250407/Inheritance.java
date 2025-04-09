b879db232516b0fd26f444c4c84a120cfad15b92
Kim Min Gyu <dooek1318@g.yju.ac.kr>
2025-04-09 15:51:09 +0900

    250407 상속

---
+++ src/OOP_250407/Inheritance.java
@@ -0,0 +1,44 @@
+//package OOP_250407;
+//
+////class ElectronicDevice {}
+////
+////class Computer extends ElectronicDevice{}
+////class Laptop extends Computer{}
+////class Desktop extends Computer{}
+////
+////class Smartphone extends ElectronicDevice{}
+////class AndroidPhone extends Smartphone {}
+////class Iphone extends Smartphone{}
+////
+//
+//class A  extends Object{ // 최 상위 클래스는 Object에서 상속받음
+//    A() {
+//        System.out.println("A 생성자 호출!");
+//    }
+//}
+//
+//class B extends A {
+//    B(int m) {
+//        System.out.println("B 생성자 호출");
+//    }
+//}
+//
+//class C extends B {
+//    C() {
+//        System.out.println("C 생성자 호출");
+//    }
+//    C(int m){
+//        this();
+//        System.out.println("C int m 생성자 호출");
+//    }
+//    C(int m, int n) {
+//        this();
+//        System.out.println("C int m, int n 생성자 호출");
+//    }
+//}
+//
+//public class Inheritance {
+//    public static void main(String[] args) {
+//        new C();
+//    }
+//}
