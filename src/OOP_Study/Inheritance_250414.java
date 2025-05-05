ed624d0fe706042ce3d9e66cda4054f4a361242e
Kim Min Gyu <dooek1318@g.yju.ac.kr>
2025-05-06 00:32:39 +0900

    시험공부

--- src/OOP_Study/Inheritance_250414.java
+++ src/OOP_Study/Inheritance_250414.java
@@ -1,34 +1,34 @@
-package OOP_Study;
-
-class A {
-    int a = 1;
-    void prt () {
-        System.out.println(a);
-    }
-}
-
-class B extends A {
-    int a = 2;
-    void prt () {
-        System.out.println(a);
-    }
-}
-
-class C extends B {
-    int a = 3;
-    void prt () {
-        System.out.println(a);
-    }
-}
-
-public class Inheritance_250414 {
-    public static void main(String[] args) {
-        A a = new A();
-        B b = new B();
-        C c = new C();
-
-        a.prt();
-        b.prt();
-        c.prt();
-    }
-}
+//package OOP_Study;
+//
+//class A {
+//    int a = 1;
+//    void prt () {
+//        System.out.println(a);
+//    }
+//}
+//
+//class B extends A {
+//    int a = 2;
+//    void prt () {
+//        System.out.println(a);
+//    }
+//}
+//
+//class C extends B {
+//    int a = 3;
+//    void prt () {
+//        System.out.println(a);
+//    }
+//}
+//
+//public class Inheritance_250414 {
+//    public static void main(String[] args) {
+//        A a = new A();
+//        B b = new B();
+//        C c = new C();
+//
+//        a.prt();
+//        b.prt();
+//        c.prt();
+//    }
+//}
---
+++ src/OOP_Study/examTest.java
@@ -0,0 +1,29 @@
+package OOP_Study;
+
+class A { int value = 1; }
+class B extends A {
+    int value;
+    B(int value) {
+        this.value = value;
+    }
+}
+class C extends A { int value = 3; }
+class D extends B {
+    int value = 4;
+    D() {
+        super(3);
+    }
+    void prtValue() {
+        System.out.println(super.value);
+        System.out.println(value);
+    }
+}
+
+public class examTest {
+    public static void main(String[] args) {
+        C c = new C();
+        D d = new D();
+
+        d.prtValue();
+    }
+}
