de56375d64d9ed143e955dd6363894e043995cae
Kim Min Gyu <dooek1318@g.yju.ac.kr>
2025-05-19 15:57:03 +0900

    Add Exception2.java - 예외 처리 예제 추가

---
+++ src/OOP_250519/Exception2.java
@@ -0,0 +1,18 @@
+package OOP_250519;
+
+import java.io.IOException;
+import java.security.cert.CertificateException;
+
+class Bar {
+    void prt() {
+        throw new IOException();
+    }
+}
+
+public class Exception2 {
+    public static void main(String[] args) throws Exception{
+
+        throw new Exception();
+
+    }
+}
