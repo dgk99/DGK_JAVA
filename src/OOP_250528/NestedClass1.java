09496399ea2526d4d6e291379ea6434440b823b4
Kim Min Gyu <dooek1318@g.yju.ac.kr>
2025-05-28 15:53:05 +0900

    250528 중첩 클래스 수업

---
+++ src/OOP_250528/NestedClass1.java
@@ -0,0 +1,24 @@
+package OOP_250528;
+
+class Board{
+    // Member Inner Class
+    class Display { }
+
+    { // Local Inner Class
+        class Test { }
+    }
+
+    void algorithm() {
+        // Local Inner Class
+        class hard {
+
+        }
+    }
+}
+
+public class NestedClass1 {
+    public static void main(String[] args) {
+
+
+    }
+}
