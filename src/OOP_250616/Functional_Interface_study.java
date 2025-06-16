62b09959da11f8fdb5c080a599728501d932f50c
Kim Min Gyu <dooek1318@g.yju.ac.kr>
2025-06-17 00:01:55 +0900

    250616 Lambda Expression, Function Interface 2 공부

---
+++ src/OOP_250616/Functional_Interface_study.java
@@ -0,0 +1,16 @@
+package OOP_250616;
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+class Add {
+    static Object add(int a, int b) { return (a + b); }
+}
+
+public class Functional_Interface_study {
+    public static void main(String[] args) {
+
+        Integer myValue = 10; // Auto-boxing
+//        Integer myValue = new Integer(10);
+        int myAge = myValue; // Unboxing
+    }
+}
